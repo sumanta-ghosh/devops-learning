f67b4dac7209849c402f3a7c0abd48940c815217
Sumanta <sumanta@SKG-Acer.localdomain>
2021-10-28 10:37:08 +0530

    add plabook for php and apache server

---
+++ ansible/lamp/readme.md
@@ -0,0 +1,1 @@
+#Install LAMP stack using ansible roles
---
+++ ansible/lamp/roles/php/files/ncoderslab.com/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>nCoders Lab</title>
+        <link rel="stylesheet" type="text/css" href="/media/main.css">
+    </head>
+    <body>
+        <div class="flex-container">
+            <div class="row">             
+                <div class="flex-item"><h1>Welcome to ncoderslab.com</h1></div>
+                <div class="flex-item"><img width="200" src="/media/app-gateway.png"></div>
+                <div class="flex-item">Host: <?php echo gethostname(); ?></div>
+            </div>
+        </div>    
+    </body>
+</html>
